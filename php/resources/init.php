fe33c8502fb8b06bb7b23771de8db5f6c08c2bb9
Patrik Bogren <pabo1800@student.miun.se>
2020-05-26 21:26:16 +0200

    Throw in some security headers

--- php/resources/init.php
+++ php/resources/init.php
@@ -15,6 +15,30 @@ session_start([
     'cookie_secure' => true     // Only send cookie over https
 ]);
 
+// TODO look at https://github.com/paragonie/csp-builder
+// Only allow scripts from this domain to run
+header("Content-Security-Policy: script-src https://127.0.0.1:*");
+
+// Only send full referrer information (where the request came from) to this site
+// over https, and only domain name to other sites over https. Any http request
+// to both this and other sites will not contain referrer information at all.
+header('Referrer-Policy: strict-origin-when-cross-origin');
+
+// Use HTTPS for future requests.
+header('Strict-Transport-Security: max-age=63072001');
+
+// Enforce mime-types on <style> and <script> tags or block the request
+// Also enables Cross-Origin Read Blocking (CORB) protection
+// Note: might have to be replaced for file downloads
+header('X-Content-Type-Options: nosniff');
+
+// Disallow <frames> to avoid Click Hijacking
+header('X-Frame-Options: Deny');
+
+// Enable extra XSS protection in browser.
+header('X-XSS-Protection: 1; mode=block');
+
+
 Session::init(new WebSession());  // Init a static session klass
 
 
