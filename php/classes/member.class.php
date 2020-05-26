bd81fbf9186c34599a9575bb45de1176a11f0fbe
Patrik Bogren <pabo1800@student.miun.se>
2020-05-26 10:08:14 +0200

    Add login and regenerate session id for login/logout

--- php/classes/member.class.php
+++ php/classes/member.class.php
@@ -154,7 +154,14 @@ class Member
         }
 
         Session::set('userid', $row['id']);
+        session_regenerate_id();
         return static::fromRow($db, $row);
+    }
+
+    public static function logout(): void
+    {
+        Session:unset('userid');
+        session_regenerate_id();
     }
 
     /**
