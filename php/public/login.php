fd6213d3fe8853162730812ce07f309029d29793
johan <jkarlsson84@gmail.com>
2020-05-28 00:11:19 +0200

    add InvReq class update login to new stucture and using Using InvReq

--- php/public/login.php
+++ php/public/login.php
@@ -10,29 +10,44 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../resources/init.php';
 
+function sendResponse($responseText)
+{
+    header('Content-Type: application/json');
+    echo json_encode($responseText);
+}
+
 $responseText = [];
+
+if (!InvReq::validIpCurUser()) {
+    sendResponse('Ip blocked');
+    exit;
+}
+
 
 if (!isset($_POST["token"]) || !isset($_POST["TS"])) {
     // Cross reference protection not provided
     ///@todo decide action
-
-    $responseText["msg"] = "Required login data not provided";
-} else {
-    if (Token::validateToken("login", $_POST["TS"], $_POST["token"])) {
-        $member = Member::login($_POST["uname"], $_POST['psw']);
-
-        // Set response data
-        $responseText["isValidLogin"] = !$member->error();
-        if (is_null($member->errorMessage())) {
-            $responseText['msg'] = "";
-        } else {
-            $responseText['msg'] = $member->errorMessage();
-        }
-    } else {
-        ///@todo decide invalid token message
-        $responseText['msg'] = "Invalid token";
-    }
-    // Send back response
+    sendResponse("Required login data not provided");
+    exit;
 }
-header('Content-Type: application/json');
-echo json_encode($responseText);
+
+if (!Token::validateToken("login", $_POST["TS"], $_POST["token"])) {
+    sendResponse("Invalid token");
+    exit;
+}
+
+$member = Member::login($_POST["uname"], $_POST['psw']);
+
+// Set response data
+$responseText["isValidLogin"] = !$member->error();
+if (!is_null($member->errorMessage())) {
+    $responseText['msg'] = "";
+} else {
+    $responseText['msg'] = $member->errorMessage();
+}
+sendResponse($responseText);
+
+
+
+
+
