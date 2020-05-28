af9139fc7afd31f8bb34fb6ddb03ff38e1f85f10
Fredrik Helgesson <frhe0300@student.miun.se>
2020-05-28 15:43:00 +0200

    Lagt till Henriks kod som inte följde med. Ändrat origin-policy(verkar blivit fel). Lagt tillbaka cdnj länkar för jquery och font-awsome för att felsöka resten.

--- php/public/search.php
+++ php/public/search.php
@@ -33,9 +33,11 @@ if (isset($_GET["search-type"]) && isset($_GET["search-field"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
-    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <!-- <link rel="stylesheet" href="css/all.min.css"> -->
     <link rel="stylesheet" href="css/style.css" />
-    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <script src="js/jquery-3.5.1.min.js"></script> -->
     <script src="js/main.js"></script>
 </head>
 
