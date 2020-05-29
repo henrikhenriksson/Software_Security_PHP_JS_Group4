e65fee577401eb5d8bcbdbde539c4a4dcde916b3
johan <jkarlsson84@gmail.com>
2020-05-29 16:10:11 +0200

    Added generated token at aside login

--- php/resources/views/aside-login.php
+++ php/resources/views/aside-login.php
@@ -19,20 +19,12 @@ $logoutClass = Member::loggedIn() ? "" : "hide";
 <div id="login" class="<?php echo $loginClass; ?>">
     <h2>LOGIN</h2>
     <form id="loginForm">
-        <?php try {
-            $token->insertToken('/public/login.php');
-        } catch (Exception $e) {
-            ///@todo link to page https://www.monkeyuser.com/2017/http-status-codes/
-        } ?>
+        <?php $token->insertToken('./login.php') ?>
         <label><b>Username</b></label>
         <input type="text" placeholder="m" name="uname" id="uname" required maxlength="10" value="m" autocomplete="off">
-        <label>
-            <p><b>Password</b></p>
-        </label>
+        <label><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-        <br>
         <button type="button" id="loginButton">Login</button>
-        <span id="loginMsg" class="red"></span>
     </form>
     <h3>Not A Member?</h3>
     <p>
@@ -43,3 +35,4 @@ $logoutClass = Member::loggedIn() ? "" : "hide";
     <h2>LOGOUT</h2>
     <button type="button" id="logoutButton">Logout</button>
 </div>
+<p id="loginMsg"></p>
