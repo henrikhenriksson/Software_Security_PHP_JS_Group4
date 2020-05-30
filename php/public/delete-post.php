fb5a41bd7a28fedcdb290e6b543a57dbee373547
johan <jkarlsson84@gmail.com>
2020-05-30 22:40:05 +0200

    update check delete-post.php

--- php/public/delete-post.php
+++ php/public/delete-post.php
@@ -2,16 +2,40 @@
 
 require_once __DIR__ . '/../resources/init.php';
 
-$response = "false";
+use \ParagonIE\AntiCSRF\AntiCSRF as TokenLib;
 
-if (isset($_POST['post_id']) && isset($_POST['token']) && isset($_POST['ts'])) {
+$token = new TokenLib();
+$member = Member::fromSession();
 
-    if (Token::validateToken("login", $_POST["ts"], $_POST["token"])) {
-        if (Post::deletePost($_POST['post_id'])) {
-            $response = "true";
-        }
-    }
+function sendResponse( bool $valid)
+{
+    $response = $valid? "true" : "false";
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
 
-header('Content-Type: application/json');
-echo json_encode($response);
+if (!isset($_POST['post_id'])) {
+    // Expecting this as an unintended call since no data to modify is supplied
+    sendResponse(false);
+    exit;
+}
+
+if(  ($member->id() <= 0) )
+{
+    InvReq::addInvalidRequest('delete_post_no_user', 'NA');
+    sendResponse(false);
+    exit;
+}
+
+if(!$token->validateRequest()) {
+    InvReq::addInvalidRequest('delete_post_invalid_token', $member->username());
+    sendResponse(false);
+    exit;
+}
+
+// Valid verification data, return success of operation
+sendResponse(Post::deletePost($_POST['post_id']));
+
+
+
+
